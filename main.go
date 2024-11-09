0622ffeae6c5b7fb5054696d1fb69d1248b8f3d5
Ahmet Alp Balkan <ahmet@linkedin.com>
2024-11-09 14:00:59 -0800

    step5

--- main.go
+++ main.go
@@ -11,18 +11,33 @@ import (
 func main() {
 	configFlags := genericclioptions.NewConfigFlags(true)
 	configFlags.AddFlags(pflag.CommandLine)
+
+	// 1. Define the namespace flag
+	allNamespaces := pflag.BoolP("all-namespaces", "A", false, "If present, list the requested object(s) across all namespaces.")
 	pflag.Parse()
 
+	// 2. Read the namespace preference from kubeconfig
+	kubeconfigNamespace, _, err := configFlags.ToRawKubeConfigLoader().Namespace()
+	if err != nil { panic(err) }
+
+	var namespace string
+	if *configFlags.Namespace != "" {
+		namespace = *configFlags.Namespace // -n flag takes precedence
+	} else if kubeconfigNamespace != "" {
+		namespace = kubeconfigNamespace // kubeconfig namespace is used if -n flag is not set
+	}
+
+
 	builder := resource.NewBuilder(configFlags)
-	err := builder.AllNamespaces(true).
+	err = builder.
+		NamespaceParam(namespace).DefaultNamespace().AllNamespaces(*allNamespaces).
 		Unstructured().
 		ResourceTypeOrNameArgs(true, pflag.Args()...).
 		Flatten().
 		Do().Visit(func(info *resource.Info, err error) error {
 			if err != nil { panic(err) }
 
-			fmt.Printf("namespace=%s, name=%s, obj=%T\n",
-				info.Namespace, info.Name, info.Object)
+			fmt.Printf("namespace=%s, name=%s\n", info.Namespace, info.Name)
 			return nil
 		})
 	if err != nil { panic(err) }
